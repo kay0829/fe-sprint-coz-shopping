313d8288558c2d58f48b9ef2a693f2b84350c84d
kay0829 <eluka97@daum.net>
2023-07-07 20:09:02 +0900

    🔧 DESIGN 폰트, 컬러 등 전역적으로 사용할 스타일 tailwindcss config에 추가

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,28 @@
 export default {
     content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
     theme: {
-        extend: {},
+        extend: {
+            fontFamily: {
+                sans: ["Poppins, sans-serif"],
+                inter: ["Inter, sans-serif"],
+            },
+            fontSize: {
+                "3.5xl": "2rem",
+            },
+            spacing: {
+                "76px": "4.75rem",
+                "55px": "3.43rem",
+                "30px": "1.875rem",
+            },
+            colors: {
+                "light-black": "#ffffffa",
+                background: "#888888",
+                violet: "#452CDD",
+            },
+            borderWidth: {
+                "16px": "1rem",
+            },
+        },
     },
     plugins: [],
 };
