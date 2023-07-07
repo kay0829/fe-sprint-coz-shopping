73dd066d9ecda7c37fa4359a84be881febb54792
kay0829 <eluka97@daum.net>
2023-07-07 23:28:50 +0900

    🔨 FEAT 2) 메뉴 버튼 클릭 시 드롭다운 메뉴 내려오도록 설정

--- src/component/Common/CDropDown.tsx
+++ src/component/Common/CDropDown.tsx
@@ -1,16 +1,24 @@
 import React from "react";
+import { dropdownShow } from "@recoil/Global";
+import { useRecoilValue } from "recoil";
 
-function CDropDown({ children }: { children: JSX.Element }) {
-    const olDefaultCss = "w-max relative top-1 -left-36 shadow-md rounded-xl border border-light-black";
-    const triangleBeforeCss = `before:content-[''] before:absolute before:w-0 before:h-0 before:z-20 before:-top-3 before:left-2
+function CDropDown({ height, children }: { height: string, children: JSX.Element }) {
+    const show = useRecoilValue(dropdownShow);
+
+    const triangleBeforeCss = `before:content-[''] before:absolute before:w-0 before:h-0 before:z-30 before:-top-0 before:left-3/4
         before:border-t-0 before:border-b-16px before:border-b-light-black
         before:border-l-8 before:border-l-transparent before:border-r-8 before:border-r-transparent`;
-    const triangleAfterCss = `after:content-[''] after:absolute after:w-0 after:h-0 after:z-30 after:-top-2 after:left-2
+    const triangleAfterCss = `after:content-[''] after:absolute after:w-0 after:h-0 after:z-40 after:top-1 after:left-3/4
         after:border-t-0 after:border-b-16px after:border-b-white
         after:border-l-8 after:border-l-transparent after:border-r-8 after:border-r-transparent`;
 
+    const olDefaultCss = "w-max mt-4 shadow-md rounded-xl border border-light-black";
+
     return (
-        <div className={`absolute ${triangleBeforeCss} ${triangleAfterCss}`}>
+        <div
+            style={{ height: `${show ? height : "0"}`, transition: "height ease-out 500ms 0s" }}
+            className={`absolute z-20 right-52px w-max overflow-hidden ${triangleBeforeCss} ${triangleAfterCss}`}
+        >
             <ol className={`${olDefaultCss}`}>{children}</ol>
         </div>
     );
