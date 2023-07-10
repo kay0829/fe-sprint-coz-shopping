01b5ae59811737515ef939b51a43dce1d9d1faa5
kay0829 <eluka97@daum.net>
2023-07-10 15:14:41 +0900

    💄 DESIGN 4) Gnb 아이템에 호버 스타일링과 클릭된 요소 스타일링

--- src/component/Global/Gnb.tsx
+++ src/component/Global/Gnb.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 
 import IconAll from "@asset/icon-all.png";
 import IconProduct from "@asset/icon-product.png";
@@ -7,6 +7,7 @@ import IconDisplay from "@asset/icon-display.png";
 import IconBrand from "@asset/icon-category.png";
 
 interface IGnbItems {
+    id: number;
     label: string;
     img: string;
     isSelected: boolean;
@@ -15,37 +16,62 @@ interface IGnbItems {
 function Gnb() {
     const gnbItems: Array<IGnbItems> = [
         {
+            id: 0,
             label: "전체",
             img: IconAll,
             isSelected: true,
         },
         {
+            id: 1,
             label: "상품",
             img: IconProduct,
             isSelected: false,
         },
         {
+            id: 2,
             label: "카테고리",
             img: IconCategory,
             isSelected: false,
         },
         {
+            id: 3,
             label: "기획전",
             img: IconDisplay,
             isSelected: false,
         },
         {
+            id: 4,
             label: "브랜드",
             img: IconBrand,
             isSelected: false,
         },
     ];
 
+    const [gnbMenu, setGnbMenu] = useState(gnbItems);
+
     return (
         <div className="flex justify-center">
-            {gnbItems.map((v: IGnbItems, i: number) => {
+            {gnbMenu.map((v: IGnbItems, i: number) => {
                 return (
-                    <div key={v.label} className={`${i !== gnbItems.length - 1 ? "mr-9" : ""} cursor-pointer`}>
+                    <div
+                        key={v.label}
+                        className={`${
+                            i !== gnbItems.length - 1 ? "mr-9" : ""
+                        } cursor-pointer hover:font-bold hover:text-violet`}
+                        onClick={() => {
+                            const selectedIdx = i;
+
+                            const temp = gnbMenu.map((v) => {
+                                if (v.id === selectedIdx) {
+                                    return { ...v, isSelected: true };
+                                } else {
+                                    return { ...v, isSelected: false };
+                                }
+                            });
+
+                            setGnbMenu(temp);
+                        }}
+                    >
                         <img src={v.img} alt="" className="w-82px h-82px mb-1.5" />
 
                         <div className="text-center">
