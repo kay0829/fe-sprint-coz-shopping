67cf2110c4ddb476eb91d6dfc7b31e939102c4d2
kay0829 <eluka97@daum.net>
2023-07-10 13:41:11 +0900

    💄 FEAT 3) Header와 Footer 뺀 부분인 main 태그 영역이 스크린 영역보다 짧을 경우 Footer를 하단에 고정 시키기

--- src/container/Container.tsx
+++ src/container/Container.tsx
@@ -1,12 +1,41 @@
-import React from "react";
+import React, { useRef, RefObject, useState, useEffect } from "react";
 import { Outlet } from "react-router";
 import Header from "@component/Global/Header";
+import Footer from "@component/Global/Footer";
+
+import { useElementWidthAndHeight } from "@hook/useElementWidthAndHeight";
+import { useSceenWidthAndHeight } from "@hook/useScreenWidthAndHeight";
 
 function Container() {
+    // prettier-ignore
+    const mainRef: RefObject<HTMLElement> = useRef<HTMLElement>(null);
+
+    const screenRect = useSceenWidthAndHeight();
+    const elementRect = useElementWidthAndHeight(mainRef);
+
+    const [isNeededToBeFixed, setIsNeededToBeFixed] = useState(false);
+
+    useEffect(() => {
+        // main 의 height가 짧을 경우 Footer 를 하단에 고정시키기 위함
+        // screen.height: 스크린의 height
+        // main.height: Header와 Footer를 제외한 main 엘리먼트의 전체 height
+        // 80: Header의 height
+        // 58: Footer의 height
+        const temp = screenRect.height - elementRect.height - 80 - 58;
+        if (temp && temp > 0) {
+            setIsNeededToBeFixed(true);
+        } else {
+            setIsNeededToBeFixed(false);
+        }
+    }, [elementRect.height, screenRect.height]);
+
     return (
         <>
             <Header />
-            <Outlet />
+            <main ref={mainRef}>
+                <Outlet />
+            </main>
+            <Footer isNeededToBeFixed={isNeededToBeFixed} />
         </>
     );
 }
