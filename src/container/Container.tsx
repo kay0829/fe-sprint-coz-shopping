e75b1842ff873aeb0ac1f5495545b87034031357
kay0829 <eluka97@daum.net>
2023-07-10 17:22:24 +0900

    🐛 FIX 5) 컴포넌트가 mount되기 전 재렌더링하려고할 경우 에러가 발생 -> React.Suspense 로 해결

--- src/container/Container.tsx
+++ src/container/Container.tsx
@@ -3,7 +3,6 @@ import { Outlet } from "react-router";
 import Header from "@component/Global/Header";
 import Footer from "@component/Global/Footer";
 
-import { useElementWidthAndHeight } from "@hook/useElementWidthAndHeight";
 import { useSceenWidthAndHeight } from "@hook/useScreenWidthAndHeight";
 
 function Container() {
@@ -16,9 +15,8 @@ function Container() {
     const [mainHeight, setMainHeight] = useState<number>(0);
 
     useEffect(() => {
-        // main 의 height가 짧을 경우 Footer 를 하단에 고정시키기 위함
+        // main 의 min-height는 screen 높이에서 Header와 Footer를 제외한 높이
         // screen.height: 스크린의 height
-        // main.height: Header와 Footer를 제외한 main 엘리먼트의 전체 height
         // 80: Header의 height
         // 58: Footer의 height
         const temp = screenRect.height - 80 - 58;
@@ -29,7 +27,9 @@ function Container() {
         <>
             <Header />
             <main ref={mainRef} style={{ minHeight: mainHeight + "px" }}>
-                <Outlet />
+                <React.Suspense fallback={<div>Loading...</div>}>
+                    <Outlet />
+                </React.Suspense>
             </main>
             <Footer />
         </>
