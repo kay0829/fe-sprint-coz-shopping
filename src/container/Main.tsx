94cc1c60b7bd224f9e0c98ddea67cab32ac413d5
kay0829 <eluka97@daum.net>
2023-07-10 17:17:51 +0900

    🔨 FEAT 5) Main 컴포넌트에서 product list 가져오기

--- src/container/Main.tsx
+++ src/container/Main.tsx
@@ -1,6 +1,11 @@
 import React from "react";
+import { useRecoilValue } from "recoil";
+
+import { reqGetProductList } from "@recoil/ProductList/index";
 
 function Main() {
+    const productList = useRecoilValue(reqGetProductList);
+
     return <p className="text-3xl font-bold underline">Hello world!</p>;
 }
 
