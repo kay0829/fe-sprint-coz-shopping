5e0bee677d519abddbeb6374fe57dd9367fb1c23
kay0829 <eluka97@daum.net>
2023-07-13 15:07:29 +0900

    🔨 FEAT 9) 상품 리스트가 없을 경우 대비하여 컴포넌트 분기

--- src/container/ProductList.tsx
+++ src/container/ProductList.tsx
@@ -7,22 +7,29 @@ import Gnb from "@component/Global/Gnb";
 
 function ProductList({ isMain }: { isMain: boolean }) {
     const productList = useRecoilValue(reqGetProductList);
+    const NoProductList = () => (
+        <div className="flex justify-center items-center w-screen h-210px">상품이 없습니다 🥲</div>
+    );
 
     return (
         <>
             {isMain ? (
                 <div className="flex flex-nowrap w-full overflow-x-scroll">
-                    {productList.map((v) => (
-                        <CProductItem item={v} key={v.id} />
-                    ))}
+                    {productList.length > 0 ? (
+                        productList.slice(0, 10).map((v) => <CProductItem item={v} key={v.id} />)
+                    ) : (
+                        <NoProductList />
+                    )}
                 </div>
             ) : (
                 <div>
                     <Gnb />
                     <div className="flex flex-wrap justify-center">
-                        {productList.map((v) => (
-                            <CProductItem item={v} key={v.id} />
-                        ))}
+                        {productList.length > 0 ? (
+                            productList.map((v) => <CProductItem item={v} key={v.id} />)
+                        ) : (
+                            <NoProductList />
+                        )}
                     </div>
                 </div>
             )}
