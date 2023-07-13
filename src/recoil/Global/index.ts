feec5a799dcf611f1be77f4e1c4ee8f25ce759ce
kay0829 <eluka97@daum.net>
2023-07-13 10:47:32 +0900

    🔧 CHORE 8) 토스트 리스트 상태를 저장할 atom 추가 및 atom 상태를 변경할 select 작성

--- src/recoil/Global/index.ts
+++ src/recoil/Global/index.ts
@@ -1,6 +1,30 @@
-import { atom } from "recoil";
+import { DefaultValue, atom, selector, selectorFamily } from "recoil";
+import { Toast } from "@type/Global";
 
-export const dropdownShow = atom({
+
+export const dropdownShow = atom<boolean>({
     key: "dropdownShow",
     default: false,
 });
+
+export const toastState = atom<Toast[]>({
+  key: 'toastState',
+  default: [],
+});
+// 기능 1. toastItem 추가
+export const addToastItem = selector({
+  key: "addToastItem",
+  get: ({ get }) => get(toastState),
+  set: ({ set }, newToast: Toast[] | DefaultValue) => {
+    set(toastState, prevToast => [...prevToast, ...newToast as []]);
+  },
+})
+// 기능 2. id에 해당하는 toastItem 삭제
+export const removeToastItem = selector({
+  key: "removeToastItem",
+  get: ({ get }) => get(toastState),
+  set: ({ set }, deletedToast: Toast[] | DefaultValue) => {
+      const deleteId = Array.isArray(deletedToast) ? deletedToast[0]?.id : undefined;
+      set(toastState, prevToast => prevToast.filter((v: Toast) => v.id && v.id !== deleteId));
+  },
+})
