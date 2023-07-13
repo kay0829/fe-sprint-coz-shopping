f9c5777fbc385e7ae62753f0a36d07da0f1bce9d
kay0829 <eluka97@daum.net>
2023-07-13 16:02:20 +0900

    🔨 FEAT 9) 북마크 컴포넌트 추가

---
+++ src/container/BookmarkList.tsx
@@ -0,0 +1,45 @@
+import React from "react";
+import { useRecoilValue } from "recoil";
+import { productItemWithBookmark } from "@recoil/Bookmark";
+
+import CProductItem from "@component/Common/CProductItem";
+import Gnb from "@component/Global/Gnb";
+
+function BookmarkList({ isMain }: { isMain: boolean }) {
+    const bookmarkList = useRecoilValue(productItemWithBookmark);
+
+    const NoBookmarkList = () => (
+        <div className="flex justify-center items-center w-screen h-210px">북마크된 상품이 없습니다 🥲</div>
+    );
+
+    return (
+        <>
+            {isMain ? (
+                <div
+                    className={`flex flex-nowrap w-full ${
+                        bookmarkList && bookmarkList.length > 4 ? "overflow-x-scroll" : ""
+                    }`}
+                >
+                    {bookmarkList.length > 0 ? (
+                        bookmarkList.slice(0, 10).map((v) => <CProductItem item={v} key={v.id} />)
+                    ) : (
+                        <NoBookmarkList />
+                    )}
+                </div>
+            ) : (
+                <div>
+                    <Gnb />
+                    <div className="flex flex-wrap justify-center">
+                        {bookmarkList.length > 0 ? (
+                            bookmarkList.map((v) => <CProductItem item={v} key={v.id} />)
+                        ) : (
+                            <NoBookmarkList />
+                        )}
+                    </div>
+                </div>
+            )}
+        </>
+    );
+}
+
+export default BookmarkList;
