2051f7eb656c0c849fabdbfc2ad3fa9b2d215fb8
kay0829 <eluka97@daum.net>
2023-07-13 10:53:50 +0900

    🔨 FEAT 8) 토스트 개수만큼 map 함수로 토스트 컴포넌트를 렌더링할 토스트 리스트 컴포넌트 추가

---
+++ src/component/Common/CToastList.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { useRecoilValue } from "recoil";
+import { toastState } from "@recoil/Global";
+
+import CToast from "@component/Common/CToast";
+
+function CToastList() {
+    const toasts = useRecoilValue(toastState);
+
+    return (
+        <div className="fixed bottom-10 right-4">
+            {toasts.map((v) => (
+                <CToast key={v.id} content={v.content} bottom={v.bottom} duration={v.duration} />
+            ))}
+        </div>
+    );
+}
+
+export default CToastList;
