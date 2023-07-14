b6f91cdcee7aeb244eac45523b3853f74c8cc05d
kay0829 <eluka97@daum.net>
2023-07-14 17:14:22 +0900

    ♻️ REFACTOR 10) 상품 아이템 컴포넌트에서 북마크 버튼 이벤트 핸들러 수정

--- src/component/Common/CProductItem.tsx
+++ src/component/Common/CProductItem.tsx
@@ -1,6 +1,7 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 
 import { useSetRecoilState } from "recoil";
+import { changeIsBookmarkedStatus } from "@recoil/ProductList";
 import { addBookmark, removeBookmark } from "@recoil/Bookmark";
 
 import CModal from "@component/Common/CModal";
@@ -17,16 +18,35 @@ function CProductItem(props: { item: IProductItemWithBookmark }) {
     const { id, type, isBookmarked } = props.item;
 
     const [isOpen, setIsOpen] = useState(false);
-    const [bookmarked, setBookmarked] = useState(!!isBookmarked);
 
     const { fireToast } = useToast();
     const addBookmarkFn = useSetRecoilState(addBookmark);
     const removeBookmarkFn = useSetRecoilState(removeBookmark);
 
+    const changeIsBookmarkedStatusFn = useSetRecoilState(changeIsBookmarkedStatus(id));
 
+    const productInfo = useProductInfo({ item: props.item });
 
+    const handleBookmarkClick = (e: React.MouseEvent<HTMLElement>) => {
+        e.stopPropagation();
 
+        let content = { color: "", text: "" };
+        if (isBookmarked) {
+            removeBookmarkFn([{ ...props.item }]);
+            content = { color: "#e8e8e8", text: "상품이 북마크에서 삭제되었습니다" };
+        } else {
+            addBookmarkFn([{ ...props.item, isBookmarked: true }]);
+            content = { color: "#FFD361", text: "상품이 북마크에 추가되었습니다" };
         }
+        fireToast({
+            content: (
+                <div className="flex items-center">
+                    <AiFillStar size={"2rem"} color={content.color} />
+                    <p>{content.text}</p>
+                </div>
+            ),
+        });
+        changeIsBookmarkedStatusFn([]);
     };
 
     return (
@@ -51,36 +71,8 @@ function CProductItem(props: { item: IProductItemWithBookmark }) {
                     <p>{productInfo?.subTitle || ""}</p>
                     <p>{productInfo?.subInfo || ""}</p>
                 </div>
-                <button
-                    className="absolute bottom-16 right-3"
-                    onClick={(e) => {
-                        e.stopPropagation();
-                        if (bookmarked) {
-                            removeBookmarkFn([{ ...props.item }]);
-                            setBookmarked(false);
-                            fireToast({
-                                content: (
-                                    <div className="flex items-center">
-                                        <AiFillStar size={"2rem"} color="#e8e8e8" />
-                                        <p>상품이 북마크에서 제거되었습니다.</p>
-                                    </div>
-                                ),
-                            });
-                        } else {
-                            addBookmarkFn([{ ...props.item, isBookmarked: true }]);
-                            setBookmarked(true);
-                            fireToast({
-                                content: (
-                                    <div className="flex items-center">
-                                        <AiFillStar size={"2rem"} color="#FFD361" />
-                                        <p>상품이 북마크에 추가되었습니다.</p>
-                                    </div>
-                                ),
-                            });
-                        }
-                    }}
-                >
-                    {bookmarked ? (
+                <button className="absolute bottom-16 right-3" onClick={handleBookmarkClick}>
+                    {isBookmarked ? (
                         <AiFillStar size={"2rem"} color="#FFD361" />
                     ) : (
                         <AiFillStar size={"2rem"} color="#e8e8e8" />
