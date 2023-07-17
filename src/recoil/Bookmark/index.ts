fe42c7c16059753cabcc29bc2afb2a111ed19046
kay0829 <eluka97@daum.net>
2023-07-17 12:37:54 +0900

    🔨 STYLE 10) prettier 적용

--- src/recoil/Bookmark/index.ts
+++ src/recoil/Bookmark/index.ts
@@ -1,5 +1,5 @@
-import { DefaultValue, atom, selectorFamily, SerializableParam, selector } from "recoil";
-import { IProductItemWithBookmark, IProductItem } from "@type/ProductList";
+import { atom, selector } from "recoil";
+import { IProductItemWithBookmark } from "@type/ProductList";
 import { selectedGnbType } from "@recoil/Global";
 
 const localStorageEffect = (key: string) => ({ setSelf, onSet }: any) => {
@@ -17,30 +17,32 @@ const localStorageEffect = (key: string) => ({ setSelf, onSet }: any) => {
   };
 
 export const productItemWithBookmark = atom<IProductItemWithBookmark[]>({
-  key: "productItemWithBookmark",
-  default: [] as IProductItemWithBookmark[],
-  effects: [localStorageEffect("bookmarks")],
+    key: "productItemWithBookmark",
+    default: [] as IProductItemWithBookmark[],
+    effects: [localStorageEffect("bookmarks")],
 });
 
 export const addBookmark = selector({
-  key: "addBookmark",
-  get: ({ get }) => { 
-      return get(productItemWithBookmark);
-  },
-  set: ({ set }, newBookmark) => {
-    set(productItemWithBookmark, prevBookmark => [...prevBookmark, ...newBookmark as []]);
-  },
+    key: "addBookmark",
+    get: ({ get }) => { 
+        return get(productItemWithBookmark);
+    },
+    set: ({ set }, newBookmark) => {
+      set(productItemWithBookmark, prevBookmark => [...prevBookmark, ...newBookmark as []]);
+    },
 })
 
 export const removeBookmark = selector({
-  key: "removeBookmark",
-  get: ({ get }) => { 
-      return get(productItemWithBookmark);
-  },
-  set: ({ set }, deletedBookmark) => {
-    const deleteId = Array.isArray(deletedBookmark) ? deletedBookmark[0]?.id : undefined;
-      set(productItemWithBookmark, prevBookmark => prevBookmark.filter((v: IProductItemWithBookmark) => v.id && v.id !== deleteId));
-  },
+    key: "removeBookmark",
+    get: ({ get }) => { 
+        return get(productItemWithBookmark);
+    },
+    set: ({ set }, deletedBookmark) => {
+      const deleteId = Array.isArray(deletedBookmark) ? deletedBookmark[0]?.id : undefined;
+        set(productItemWithBookmark, prevBookmark => prevBookmark.filter((v: IProductItemWithBookmark) => v.id && v.id !== deleteId));
+    },
+})
+
 // 상품 타입에 따라 북마크 리스트 데이터를 필터하여 화면에 뿌리기 위한 get 함수
 export const filterBookmarkListByType = selector({
     key: "filterBookmarkListByType",
