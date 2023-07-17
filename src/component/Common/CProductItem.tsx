055cf2bd3a5578b88f27384f62df48ea9614fb45
kay0829 <eluka97@daum.net>
2023-07-17 17:26:40 +0900

    ♻️ FEAT 10) 북마크 버튼 공용 컴포넌트로 분리

--- src/component/Common/CProductItem.tsx
+++ src/component/Common/CProductItem.tsx
@@ -1,15 +1,10 @@
 import React, { useState } from "react";
 
-import { useSetRecoilState } from "recoil";
-import { changeIsBookmarkedStatus } from "@recoil/ProductList";
-import { addBookmark, removeBookmark } from "@recoil/Bookmark";
-
 import CModal from "@component/Common/CModal";
+import CBookmarkBtn from "@component/Common/CBookmarkBtn";
 import ProductModal from "@component/ProductList/ProductModal";
 import PreparingImage from "@asset/preparing-image.jpeg";
-import { AiFillStar } from "react-icons/ai";
 
-import { useToast } from "@hook/useToast";
 import { useProductInfo } from "@hook/useProductInfo";
 
 import { IProductItemWithBookmark } from "@type/ProductList";
@@ -19,35 +14,8 @@ function CProductItem(props: { item: IProductItemWithBookmark }) {
 
     const [isOpen, setIsOpen] = useState(false);
 
-    const { fireToast } = useToast();
-    const addBookmarkFn = useSetRecoilState(addBookmark);
-    const removeBookmarkFn = useSetRecoilState(removeBookmark);
-
-    const changeIsBookmarkedStatusFn = useSetRecoilState(changeIsBookmarkedStatus(id));
-
     const productInfo = useProductInfo({ item: props.item });
-
-    const handleBookmarkClick = (e: React.MouseEvent<HTMLElement>) => {
-        e.stopPropagation();
-
-        let content = { color: "", text: "" };
-        if (isBookmarked) {
-            removeBookmarkFn([{ ...props.item }]);
-            content = { color: "#e8e8e8", text: "상품이 북마크에서 삭제되었습니다" };
-        } else {
-            addBookmarkFn([{ ...props.item, isBookmarked: true }]);
-            content = { color: "#FFD361", text: "상품이 북마크에 추가되었습니다" };
-        }
-        fireToast({
-            content: (
-                <div className="flex items-center">
-                    <AiFillStar size={"2rem"} color={content.color} />
-                    <p>{content.text}</p>
-                </div>
-            ),
-        });
-        changeIsBookmarkedStatusFn([]);
-    };
+    const btnStyle = "absolute bottom-16 right-3";
 
     return (
         <>
@@ -71,13 +39,7 @@ function CProductItem(props: { item: IProductItemWithBookmark }) {
                     <p>{productInfo?.subTitle || ""}</p>
                     <p>{productInfo?.subInfo || ""}</p>
                 </div>
-                <button className="absolute bottom-16 right-3" onClick={handleBookmarkClick}>
-                    {isBookmarked ? (
-                        <AiFillStar size={"2rem"} color="#FFD361" />
-                    ) : (
-                        <AiFillStar size={"2rem"} color="#e8e8e8" />
-                    )}
-                </button>
+                <CBookmarkBtn btnStyle={btnStyle} isBookmarked={!!isBookmarked} item={props.item} />
             </figure>
             {isOpen ? (
                 <CModal setIsOpen={setIsOpen}>
--- src/component/ProductList/ProductModal.tsx
+++ src/component/ProductList/ProductModal.tsx
@@ -1,16 +1,10 @@
 import React, { Dispatch, SetStateAction } from "react";
 import { useSceenWidthAndHeight } from "@hook/useScreenWidthAndHeight";
 
-import { useSetRecoilState } from "recoil";
-import { changeIsBookmarkedStatus } from "@recoil/ProductList";
-import { addBookmark, removeBookmark } from "@recoil/Bookmark";
-
-import { useToast } from "@hook/useToast";
-
-import { AiFillStar } from "react-icons/ai";
 import { IoClose } from "react-icons/io5";
 
 import { IProductItemWithBookmark } from "@type/ProductList";
+import CBookmarkBtn from "@component/Common/CBookmarkBtn";
 
 function ProductModal({
     title,
@@ -27,34 +21,6 @@ function ProductModal({
 }) {
     const screenRect = useSceenWidthAndHeight();
 
-    const { fireToast } = useToast();
-    const addBookmarkFn = useSetRecoilState(addBookmark);
-    const removeBookmarkFn = useSetRecoilState(removeBookmark);
-
-    const changeIsBookmarkedStatusFn = useSetRecoilState(changeIsBookmarkedStatus(item.id));
-
-    const handleBookmarkClick = (e: React.MouseEvent<HTMLElement>) => {
-        e.stopPropagation();
-
-        let content = { color: "", text: "" };
-        if (isBookmarked) {
-            removeBookmarkFn([{ ...item }]);
-            content = { color: "#e8e8e8", text: "상품이 북마크에서 삭제되었습니다" };
-        } else {
-            addBookmarkFn([{ ...item, isBookmarked: true }]);
-            content = { color: "#FFD361", text: "상품이 북마크에 추가되었습니다" };
-        }
-        fireToast({
-            content: (
-                <div className="flex items-center">
-                    <AiFillStar size={"2rem"} color={content.color} />
-                    <p>{content.text}</p>
-                </div>
-            ),
-        });
-        changeIsBookmarkedStatusFn([]);
-    };
-
     return (
         <div
             className="relative object-cover rounded-xl overflow-hidden"
@@ -65,9 +31,7 @@ function ProductModal({
                 <IoClose size={"2rem"} color="#ffffff" />
             </button>
             <div className="flex items-center absolute bottom-3 left-3">
-                <button className="mr-2" onClick={handleBookmarkClick}>
-                    <AiFillStar size={"2rem"} color="#e8e8e8" />
-                </button>
+                <CBookmarkBtn btnStyle="mr-2" isBookmarked={isBookmarked} item={item} />
                 <span className="text-white">{title}</span>
             </div>
         </div>
